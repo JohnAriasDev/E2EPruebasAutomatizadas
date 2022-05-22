4587b5a3162e88815fa2770c8860f53e55cdad62
dacperezce96 <dc.perezc1@uniandes.edu.co>
2022-05-22 13:23:24 -0500

    32 escenarios con estrategias de generación de datos

---
+++ cypressGhostDatos/cypress/integration/GeneraciónDatos/createPostWithFacebook.spec.js
@@ -0,0 +1,42 @@
+// <reference types="cypress" />
+import LoginSelector from "../../support/PageObjects/loginSelector";
+import PostSelector from "../../support/PageObjects/postSelector"
+import { faker } from '@faker-js/faker';
+
+describe("create a post with metadata", () => {
+    let data;
+    const newPost = faker.commerce.productName();
+    const loginSelector = new LoginSelector();
+    const postSelector = new PostSelector();
+    let index = 0;
+
+  //@Given
+  beforeEach(() => {
+    Cypress.on("uncaught:exception", (err, runnable) => {
+      return false;
+    });
+    cy.fixture('datos').then(function(dataR){
+        data = dataR;
+    });
+  });
+  //@When
+  it("create a post with metadata", () => {
+    cy.visit(data.url4+data.dir);
+    loginSelector.getEmailLogin().type(data.login.email);
+    loginSelector.getPasswordLogin().type(data.login.password);
+    loginSelector.getButtonLogin().click();
+    postSelector.getNewPostButton().click();
+    postSelector.getTextAreaTitle().type(newPost);
+    postSelector.getParagraphInput().type(faker.lorem.paragraph());
+    postSelector.getSettings().click();
+    postSelector.getFacebookButton().click();
+    postSelector.getFacebookTitleInput().type(faker.lorem.paragraphs(5).substring(0, 299));
+    postSelector.getFacebookDescriptionInput().type(faker.lorem.paragraphs(5).substring(0, 499));
+    postSelector.getSettings().click();
+    postSelector.getBackButton().click();
+    postSelector.getDrafOption().click();
+  //@Then
+    postSelector.getTitleFirstElementTitle().should('to.contain',newPost);
+
+  });
+});
